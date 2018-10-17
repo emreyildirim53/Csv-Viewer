070535c2ac7403710cc65cd5be8f992d037401b9
Emre YILDIRIM <53.emreyildirim@gmail.com>
2018-10-17 12:31:35 +0300

    Proje seçme ekranı eklendi ve arayüz tasarımı yapıldı.

--- js/script.js
+++ js/script.js
@@ -1,73 +1,154 @@
 $(document).ready(function() {
 
-	//File Select
-	$('.tables').hide();
-  var objectFileList=new Array();
+  var objectFileList=new Array();//Oluşturulan her bir satır objenin tutulduğu dizi
 	var inputs = document.querySelectorAll('.inputfile');
+  var inputChangeEvnt; //İnput eventini span click fonksiyonunda kullanmak için
 
-	Array.prototype.forEach.call(inputs,function(input)
-	{
+	//*.csv file selected
+	Array.prototype.forEach.call(inputs,function(input){
 		var label	 = input.nextElementSibling,
 				labelVal = label.innerHTML;
 
 		input.addEventListener('change',function(e){
-			$('.tables').show();
-			var fileName = '';
-			if(this.files && this.files.length>1)
-				fileName = (this.getAttribute('data-multiple-caption') || '').replace('{count}', this.files.length);
-			else
-				fileName = e.target.value.split('\\').pop();
+      // Tags divi temizleme
+      var colNode=$(".tagsCols")[0];
+      while (colNode.hasChildNodes())
+          colNode.removeChild(colNode.lastChild);
 
-			if(fileName){
-				label.querySelector('span').innerHTML = fileName;
-				handleFileSelect(e);
-			}else
-				label.innerHTML = labelVal;
+      // yüklenen dosya sayısı kontrolü
+			var $fileUpload = $("input[type='file']");
+			if (parseInt($fileUpload.get(0).files.length)>2)
+			 	label.querySelector('span').innerHTML = "En fazla 2 dosya yükleyebilirsiniz.";
+		  else{
+        // tags içerisine span oluşturma (Seçilen dosya isimleri)
+        for (var i = 0; i < this.files.length; ++i) {
+          var span=$('<span>').text(this.files.item(i).name);
+          if(i==0) span.addClass('selected');
+          $(".tags").find(">:first-child").append(span);
+        }
+
+				$('.tables').show();
+				var fileName = '';
+				if(this.files && this.files.length>1)
+					fileName = (this.getAttribute('data-multiple-caption') || '').replace('{count}', this.files.length);
+				else
+					fileName = e.target.value.split('\\').pop();
+
+				if(fileName){
+					label.querySelector('span').innerHTML = fileName;
+          inputChangeEvnt=e;
+					handleFileSelect(e);
+				}else
+					label.innerHTML = labelVal;
+			}
 		});
 	});
-	//Read File
+	//Read *.csv File
 	function handleFileSelect(evt){
 	    var files = evt.target.files; // FileList object
-	    // Loop through the FileList and render image files as thumbnails.
-	    for (var i = 0, f; f = files[i]; i++)
-	    {
-	        var reader = new FileReader();
-	        reader.onload = (function(reader){
-	            return function(){
-								try{
-									var contents = reader.result;
-	                var lines = contents.split('\n');
-	                //////
-									$(".message").html("");
-									objectFileList=[];
-									lines.forEach(function(line) {
-										var datas=line.split(';');
-										if(datas.length>4) return;
-										var head=datas[0];
-										var content=datas[1];
-										var words=datas[2].replace(new RegExp(escapeRegExp(","), 'g'), ", ");
-										var betweenness=datas[3];
+      var file;
+      var uniqList=new Array();
+      var fileContList=new Array();
 
-										var objectFile={
-											head:head,
-											content:content,
-											words:words,
-											betweenness:betweenness
-										};
-										objectFileList.push(objectFile);
-									});
-									createRows();
-									//console.log(objectFileList);
-								}catch (e){
-										$(".message").html("!!Hata!!\nDosya formatı geçerli olmayabilir.\nDört sütundan oluşmalıdır.");
-								    console.log(e.stack)  // this will work on chrome, FF. will no not work on safari
-								    console.log(e.line)  // this will work on safari but not on IPhone
-								}
-	            }
-	        })(reader);
-	        reader.readAsText(f);
-	   }
+      // Seçilen tag'e göre dosya yükleme
+      for(var i=0 in files)
+        $('.tagsCols span').each(function () {
+            if(files[i].name==this.innerHTML && this.className=='selected')
+              file=files[i];
+            if(!uniqList.includes(files[i]))
+              uniqList.push(files[i]);
+        });
+
+
+      for(var i=0 in uniqList){
+        var reader = new FileReader();
+        reader.onload = (function(reader){
+                       return function(){
+                         try{
+                           fileContList.push(reader.reader);
+                           //console.log(JSON.parse(csvJSON(reader.result)));
+                           var contents = reader.result;
+                           var lines = contents.split('\n');
+
+                           //////
+                           $(".message").html("");
+                           objectFileList=[];
+                           lines.forEach(function(line) {
+                             var datas=line.split(';');
+                             if(datas.length>4) return;
+
+                             if(lines[0]==line){
+                                 $("thead").empty();
+                                 var thead=$("thead").append($('<tr>')
+                                   .addClass('table100-head')
+                                   .append($('<th>').text(datas[0]))
+                                   .append($('<th>').text(datas[1]))
+                                   .append($('<th>').text(datas[2]))
+                                   .append($('<th>').text(datas[3]))
+                                 );
+                             }else{
+
+                                 var head,content,words,betweenness;
+                                 (datas[0]) ? head=datas[0]: head="";
+                                 (datas[1]) ? content=datas[1]: content="";
+                                 (datas[2]) ? words=datas[2].replace(new RegExp(escapeRegExp(","), 'g'), ", "): words="";
+                                 (datas[3]) ? betweenness=datas[3]: betweenness="";
+
+                                 var objectFile={
+                                   head:head,
+                                   content:content,
+                                   words:words,
+                                   betweenness:betweenness
+                                 };
+                                 objectFileList.push(objectFile);
+                             }
+                           });
+                           createRows();
+                           //console.log(objectFileList);
+                         }catch (e){
+                             $(".message").html("!!Hata!!\nDosya formatı geçerli olmayabilir.\nDört sütundan oluşmalıdır.");
+                             console.log(e.stack)  // this will work on chrome, FF. will no not work on safari
+                             console.log(e.line)  // this will work on safari but not on IPhone
+                         }
+                       }
+                   })(reader);
+                   if(uniqList[i])
+        reader.readAsText(uniqList[i]);
+      }
 	}
+
+  function createJSON(fileContList){
+    console.log(fileContList);
+    for(var i=0 in fileContList)
+      console.log(fileContList[i]);
+  }
+
+  function createIdeas(csv){
+
+    var lines=csv.split('\n');
+
+    var result = [];
+
+    var headers=lines[0].split(';');
+
+    for(var i=1;i<lines.length;i++){
+
+  	  var obj = {};
+  	  var currentline=lines[i].split(';');
+
+  	  for(var j=0;j<headers.length;j++)
+  		  obj[headers[j]] = currentline[j];
+
+
+  	  result.push(obj);
+
+    }
+
+    //return result; //JavaScript object
+    return JSON.stringify(result); //JSON
+  }
+
+	//kelimeler arasına boşluk atıldı
 	function escapeRegExp(str) {
 	    return str.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
 	}
@@ -96,12 +177,11 @@ $(document).ready(function() {
 		   );
 		 });
 		}
+		$('html, body').animate({scrollTop: $(".tables").offset().top}, 1500);
 	}
+	//Tablo headerını boyutlandırma
 	$(function() {
-	    var pressed = false;
-	    var start = undefined;
-	    var startX, startWidth;
-
+	    var pressed = false,start = undefined,startX, startWidth;
 	    $("table th").mousedown(function(e) {
 	        start = $(this);
 	        pressed = true;
@@ -111,16 +191,24 @@ $(document).ready(function() {
 	    });
 
 	    $(document).mousemove(function(e) {
-	        if(pressed) {
-	            $(start).width(startWidth+(e.pageX-startX));
-	        }
+	        if(pressed) {$(start).width(startWidth+(e.pageX-startX));}
 	    });
 
 	    $(document).mouseup(function() {
-	        if(pressed) {
-	            $(start).removeClass("resizing");
-	            pressed = false;
-	        }
+	        if(pressed)$(start).removeClass("resizing");pressed = false;
 	    });
 	});
+  //Buttonlar arası geçiş işlemi
+	$(".tags").on('click','span',function() {
+    	$(this).addClass('selected').siblings().removeClass('selected');
+    	handleFileSelect(inputChangeEvnt);
+	});
+	//Network analysis buton işlevi
+	$( ".uploadNetwork" ).click(function() {
+		$('body').css("padding-bottom","100px");
+		$('html, body').animate({scrollTop: $(".bar").offset().top}, 2000);
+		$(".cards").slideUp('slow');
+		$(".bar").slideDown('slow');
+	});
+
 });
